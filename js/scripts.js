d97fabc7c8126ddfe55138d530b20bd51daeb855
Kolby Kruger <kolbykruger@gmail.com>
2020-01-20 15:26:41 -0500

    started project header design

--- js/scripts.js
+++ js/scripts.js
@@ -15,20 +15,6 @@ $('.slideshow .container').slick({
   nextArrow:'<button class="next" title="Next Slide"><i class="fal fa-chevron-right"></i></button>'
 });
 
-//Universal Slideshow
-$('.carousel .container').slick({
-  autoplay: true,
-  autoplaySpeed: 5000,
-  dots: true,
-  infinite: true,
-  fade: false,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  prevArrow:'<button class="navBtn prev" title="Previous Slide"><i class="fal fa-chevron-left"></i></button>',
-  nextArrow:'<button class="navBtn next" title="Next Slide"><i class="fal fa-chevron-right"></i></button>'
-});
-
 $('.slide').each(function(){
     $(this).removeAttr("aria-describedby");
 });
